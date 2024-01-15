9e6a036eaf0ee6f19110557a26ea0765b4234de5
alexcy1 <cyr23001@byui.edu>
2024-01-15 22:27:38 +0100

    Finished W02

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,53 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 3 - Element Variables */
+
+/* Step 2 - Variables */
+
+/* Step 4 - Adding Content */
+
+/* Step 5 - Array */
+
+
+
+// NAME -----------------------------------------------------------------------------
+const fullName = 'Alexander Cyril'
+document.querySelector('#name').textContent = fullName;
+
+// CURRENT YEAR ---------------------------------------------------------------------
+const currentYear = new Date().getFullYear();
+document.querySelector('#year').textContent = currentYear;
+
+// PROFILE IMAGE ---------------------------------------------------------------------
+const profileImage = 'images/alex.png'
+document.querySelector('img').src = profileImage;
+
+// FAVORITE FOODS---------------------------------------------------------------------
+const myFavoriteFood = ['Beans', 'Rice', 'Plantain', 'Yam', 'Potato', 'Pizza'];
+const foodElement = document.getElementById('food');
+foodElement.innerHTML = myFavoriteFood.join(', ');
+
+// Add/push a single food 
+const singleFavoriteFood = 'Ice Cream';
+myFavoriteFood.push(singleFavoriteFood);
+foodElement.innerHTML += `<br>${myFavoriteFood.join(', ')}`;
+
+// Remove the first item
+myFavoriteFood.shift();
+foodElement.innerHTML += `<br>${myFavoriteFood.join(', ')}`;
+
+// Remove the last item
+myFavoriteFood.pop();
+foodElement.innerHTML += `<br>${myFavoriteFood.join(', ')}`;
+
+
+
+
+
+
+
+
+
+
